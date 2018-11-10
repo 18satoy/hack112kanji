f534102602af0ef85fe8d3d7bfe3d126ccad92d6
Eunseo Sung <eunseos@andrew.cmu.edu>
2018-11-10 10:43:05 -0500

    dialogue text for tutorial phase

--- animations.py
+++ animations.py
@@ -18,15 +18,6 @@ class drawText(PygameGame):
     def init(self, filePath):
         self.
 
-# class myProject(PygameGame):
-#     def init(self):
-#         self.message = "world helo"
-
-#     def mousePressed(self, x, y):
-#         print(x)
-
-#     def redrawAll(self, screen):
-#         pygame.draw.rect(screen, (0,45, 200), (0, 0, self.width, self.height)) 
 
 game = drawDialogue(1000, 620)
 game.run()
