f9dc0fe3da5c8c185c547c21abdd3aa24bfea8f1
Eunseo Sung <eunseos@andrew.cmu.edu>
2018-11-10 10:37:23 -0500

    editting object classes

--- objectClasses.py
+++ objectClasses.py
@@ -5,18 +5,16 @@ from image_util import*
 import pygame
 
 class kanji (object):
-    def __init__(self, width, height, imagePath):
+    def __init__(self, imagePath):
         self.image = pygame.image.load(imagePath)
         self.image.convert()
-        self.width = width
-        self.height = height
 
     def draw(self, canvas):
     #drawing the box image in the correct position
         upperLeftX = 0
         upperLeftY = 0
-        lowerRightX = self.width #width of large canvas
-        lowerRightY = self.height #height of large canvas
+        lowerRightX = screen.width #width of large canvas
+        lowerRightY = screen.height #height of large canvas
         screen.blit(self.image, upperLeftX, upperLeftY, lowerRightX, lowerRightY)
 
 class tree (kanji):
